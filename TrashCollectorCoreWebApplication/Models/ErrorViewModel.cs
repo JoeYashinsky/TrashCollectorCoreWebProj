c56a99e845471a5098c1fa8d8766d8efe4dfa5b3
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-30 14:00:28 -0500

    initial

---
+++ TrashCollectorCoreWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrashCollectorCoreWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

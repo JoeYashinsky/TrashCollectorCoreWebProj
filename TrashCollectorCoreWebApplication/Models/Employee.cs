200a05a0e34ab319120fa651a4acc60ef5c7105d
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-30 20:04:38 -0500

    Primary Key (Id), FirstName, LastName properties added to both Customer and Employee models

--- TrashCollectorCoreWebApplication/Models/Employee.cs
+++ TrashCollectorCoreWebApplication/Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,14 @@ namespace TrashCollectorCoreWebApplication.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
 
 
         [ForeignKey("IdentityUser")]

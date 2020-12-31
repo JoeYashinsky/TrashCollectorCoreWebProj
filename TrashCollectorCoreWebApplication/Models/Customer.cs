92a4783ef6fc52fc068a239d8283e383e97179f5
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-30 19:41:27 -0500

    Added Foreign Key reference to IdentityUser in both the Customer and Employee models

--- TrashCollectorCoreWebApplication/Models/Customer.cs
+++ TrashCollectorCoreWebApplication/Models/Customer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,12 @@ namespace TrashCollectorCoreWebApplication.Models
 {
     public class Customer
     {
+
+
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }

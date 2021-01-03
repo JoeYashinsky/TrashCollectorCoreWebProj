c429c0b62b5298f25f7191d6cdcb0867295ef537
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-03 01:52:25 -0500

    utilize LINQ to formulate list for Employee of all valid Customer pickups on this particular day (Index() action in EmployeesController)

--- TrashCollectorCoreWebApplication/Controllers/EmployeesController.cs
+++ TrashCollectorCoreWebApplication/Controllers/EmployeesController.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrashCollectorCoreWebApplication.Data;
 
 namespace TrashCollectorCoreWebApplication.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: EmployeesController
         public ActionResult Index()
         {
-            return View();
+            //Access currently signed-in user. This line will return user's PK Id from AspNetUsers table. If not signed in, userId null.
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = _context.Employees.Where(c => c.IdentityUserId == userId).SingleOrDefault();
+
+            if (employee == null)
+            {
+                return RedirectToAction("Create");  //if no Id found (not an employee yet), take to Create page
+            }
+
+            //Need to filter Customers list to present this Employee with specific pickups scheduled for this day and in employee route zip
+            var routeZipCodeCustomers = _context.Customers.Where(c => c.ZipCode == employee.RouteZipCode).ToList();
+            var currentDay = DateTime.Today.DayOfWeek.ToString();
+            var regularPickupCustomers = routeZipCodeCustomers.Where(c => c.Day.Name == currentDay).ToList();
+            //Need to also account for one-time (extra) pickups or possible suspensions of service
+            var extraCustomers = regularPickupCustomers.Where(c => c.ExtraPickupDate == DateTime.Today).ToList();
+            var allCustomersToday = extraCustomers.Where(c => c.SuspendServiceDate !<= DateTime.Today && c.SuspensionEndDate !>= DateTime.Today).ToList();
+
+            return View(allCustomersToday);
         }
 
         // GET: EmployeesController/Details/5

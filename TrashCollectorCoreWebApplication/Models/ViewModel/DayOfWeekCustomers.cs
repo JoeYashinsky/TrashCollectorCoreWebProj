849d064e668ea3d40a91a3e6afc311f59656136e
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-04 18:07:54 -0500

    added viewmodel DayOfWeekCustomers for Employee to view potential Customer pickups filtered by day of the week

---
+++ TrashCollectorCoreWebApplication/Models/ViewModel/DayOfWeekCustomers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorCoreWebApplication.Models.ViewModel
+{
+    public class DayOfWeekCustomers
+    {
+
+        public List<Customer> Customers { get; set; }
+
+        public SelectList Weekdays { get; set; }
+
+
+        public string SelectedPickupDay { get; set; }
+
+    }
+}
